41769446d893e61f4a1c5175d32ba80aa07ba88a
Misha Lvovsky <m.lvovsky66@gmail.com>
2024-03-04 18:03:13 -0500

    reduce noise slightly and decrease sample expansion radius

--- latch/training/train_rollout.py
+++ latch/training/train_rollout.py
@@ -45,12 +45,12 @@ def train_rollout(train_state: LatchState) -> LatchState:
         p=1.0,
         shape=[train_state.config.traj_per_rollout],
     )
-    target_states = unit_ball_samples * train_state.config.latent_state_radius * 1.1
+    target_states = unit_ball_samples * train_state.config.latent_state_radius * 1.075
 
     # Define a function that makes a policy for a given target
     def make_policy(target: jax.Array):
         finder = FinderPolicy(latent_target=target)
-        noisy = PolicyNoiseWrapper(wrapped_policy=finder, variances=jnp.ones(2) * 0.1)
+        noisy = PolicyNoiseWrapper(wrapped_policy=finder, variances=jnp.ones(2) * 0.025)
         return noisy
 
     # Make the policy for each target
