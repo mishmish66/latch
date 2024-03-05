8f518b28faf6dc092478784b77a60588d1422502
Misha Lvovsky <m.lvovsky66@gmail.com>
2024-03-05 15:57:29 -0500

    updated init for policy module

--- latch/policy/__init__.py
+++ latch/policy/__init__.py
@@ -1,6 +1,7 @@
 from .policy import Policy
-from .optimizer_policy import OptimizerPolicy
+from .optimizer_policy import OptimizerPolicy, optimize_actions
 from .random_policy import RandomPolicy
 from .actor_policy import ActorPolicy
 from .finder_policy import FinderPolicy
 from .policy_noise_wrapper import PolicyNoiseWrapper
+from .preset_policy import PresetPolicy
